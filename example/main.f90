6ec76337ebb287ae8fae4710256bf2686712edb4
Federico E. Benelli <fedebenelli@outlook.com>
2024-03-07 15:47:51 -0300

    adding the possiblity to extract isolated properties

--- example/main.f90
+++ example/main.f90
@@ -2,11 +2,18 @@ program main
   use forsus, only: Substance, forsus_dir
 
   type(Substance) :: sus(2)
+  character(len=50) :: only_this(3)
 
   forsus_dir = "data/json"
 
+  only_this(1) = "critical"
+  only_this(2) = "unifac"
+
   sus(1) = Substance("1-butanol")
-  sus(2) = Substance("ethanol")
+  sus(2) = Substance("ethanol", path="data/json")
+
+  sus(2) = Substance("ethanol", only=["critical"])
+  sus(2) = Substance("ethanol", only=only_this)
 
   print *, sus%critical%critical_temperature%value
 end program
