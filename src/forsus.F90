1aa13ad4660f813a30b17fb946ea3956e52516d6
fedebenelli <fedebenelli@outlook.com>
2024-06-13 19:03:29 +0000

    Deploying to gh-pages from @ ipqa-research/forsus@d00f5da533cd5888eaf57ac043b4d3027da22221 🚀

--- src/forsus.F90
+++ src/forsus.F90
@@ -3,6 +3,7 @@ module forsus
     use forsus_constants
     use forsus_properties
     use forsus_substance
+    use forsus__available
 
     character(len=*), parameter :: version = VERSION
 end module
